8899a8c9e71ccbbf3493a83142e473a1447ab157
Giampaa <leangiampaolo@gmail.com>
2024-09-04 17:14:18 -0300

    feat:programa aun incompleto que togglea 3 leds en el kit respondiendo a un servidor HTML

--- actividades/ej_02/app.py
+++ actividades/ej_02/app.py
@@ -1,1 +1,23 @@
-# Aplicacion del servidor
+from boot import do_connect
+from boot import led1, led2, led3
+from microdot import Microdot, send_file
+
+do_connect()
+app = Microdot()
+
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+
+@app.route('/<dir>/<file>')
+async def index(request, dir, file):
+    return send_file("/{}/{}".format(dir, file))
+
+@app.route('/led/toggle/<led>')
+async def led_toggle(request, led):
+
+    if led == "led1":
+        led1.value(not led1.value())
+
+        #añadir el resto de ifs y probar
+app.run(port=80)
